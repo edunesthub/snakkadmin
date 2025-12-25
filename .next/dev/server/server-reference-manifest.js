1bff44691befebfed466ca21a7e4bda91402745b
youngberry1 <arimiyaobarcky01@gmail.com>
2025-12-25 06:04:31 +0000

    Stop tracking .next folder

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iTI8ADxV5RnDCdSBDQmtIZ1coy9nHeBRMPWzdCZKEyw=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__56f4a8a5._.css",
-  "static/chunks/_ce22e67c._.js"
-],
-    source: "dynamic"
-});
